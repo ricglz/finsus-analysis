0ffc69fb3a1094a50a2ff94c34cf19ea6ee38054
Ricardo J González C <rjgcastillo@hotmail.com>
2020-11-18 15:07:41 -0600

    Update to take in consideration outside investments

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ FREE_MONEY_RATE = 0.04
 INV_30_DAYS_RATE = 0.08
 INV_90_DAYS_RATE = 0.092
 INV_360_DAYS_RATE = 0.11
+MONTHLY_RATE = 0.1312
+YEARS_INVESTED = 40
+MONTHS_INVESTED = YEARS_INVESTED * 12
 
 data = []
 
@@ -14,30 +17,33 @@ def print_data():
     print(tabulate(data, headers=['Year', 'Amount']))
 
 def main():
-    free_money = 7960
-    inv_30_days = free_money
-    inv_90_days = 7961.15
-    inv_360_days = 23883.44
+    free_money = 6606.38
+    inv_30_days = 9909.57
+    inv_90_days = 15535.25
+    inv_360_days = 32146.10
     initial_investment = free_money + inv_30_days + inv_90_days + inv_360_days
     data.append([0, f'${initial_investment:,.2f}'])
 
-    years_invested = 40
-    months_invested = years_invested * 12
-
-    current_month = 1
-
     monthly_profit = lambda money, rate: money * rate / 12
 
-    while current_month <= months_invested:
+    outside_inv = 8000
+    outside_inv_profit = lambda: monthly_profit(outside_inv, MONTHLY_RATE)
+    monthly_deposits = outside_inv_profit()
+
+    for current_month in range(1, MONTHS_INVESTED + 1):
         interest_gained = monthly_profit(free_money, FREE_MONEY_RATE)
         interest_gained += monthly_profit(inv_30_days, INV_30_DAYS_RATE)
         interest_gained += monthly_profit(inv_90_days, INV_90_DAYS_RATE)
         interest_gained += monthly_profit(inv_360_days, INV_360_DAYS_RATE)
 
-        current_total = free_money + inv_30_days + interest_gained
+        current_total = free_money + inv_30_days + interest_gained + monthly_deposits
 
         if current_month % 12 == 0:
             current_total += inv_90_days + inv_360_days
+            invest = (monthly_deposits * 12 // 1000) * 1000
+            current_total -= invest
+            outside_inv += invest
+            monthly_deposits = outside_inv_profit()
             free_money = current_total * 0.025
             inv_30_days = current_total * 0.075
             inv_90_days = current_total * 0.3
@@ -52,10 +58,8 @@ def main():
             inv_30_days = current_total * 0.6
 
         if current_month % 12 == 0:
-            current_money = free_money + inv_30_days + inv_90_days + inv_360_days
+            current_money = free_money + inv_30_days + inv_90_days + inv_360_days + outside_inv
             data.append([current_month / 12, f'${current_money:,.2f}'])
-
-        current_month += 1
 
     print_data()
 
